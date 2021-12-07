20463e0dfe2c3588dec8a893296a6e6058eddd51
Shinnenijou <littlesword111@gmail.com>
2021-12-08 00:31:21 +0800

    Update record.py
    修复了读取的密码会使命令行换行而无法正确录像的bug

--- record.py
+++ record.py
@@ -1,5 +1,6 @@
 import time
 import os
+import subprocess
 
 streamer_name = ""
 record_name = input("Please input the name of streamer you want to record: ")
@@ -13,12 +14,12 @@ with open('{}/streamers.txt'.format(path)) as streamers:
         if(strs[0].upper() == record_name):
             streamer_name = strs[0]
             stream_url = strs[1]
-            file_format = strs[2]
+            file_format = strs[2].strip()
 
             # Password for Twitcasting locked stream
             password = ""
             if len(strs) > 3:
-                password += " --twitcasting-password {}".format(strs[3])
+                password += " --twitcasting-password {}".format(strs[3].strip())
             start = 1
             break
     
@@ -36,6 +37,7 @@ if streamer_name:
 while streamer_name:
     os.system("clear")
     print("Now recording: {}: {}".format(streamer_name, stream_url))
+    print()
     time_gmt8 = time.strftime("%Y%m%d_%H%M%S", time.gmtime(time.time() + 8 * 60 * 60))
     file_name = "{}/{}_{}.{}".format(save_path, streamer_name, time_gmt8, file_format)
     cmd = "streamlink {} best{} -o {}".format(stream_url, password, file_name)
