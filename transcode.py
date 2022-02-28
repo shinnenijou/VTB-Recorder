771b7e45d8b3b09b46e124cb81552e5cd0b6b6d2
Shinnenijou <littlesword111@gmail.com>
2022-03-01 00:22:29 +0800

    Update transcode.py
    修复变量名冲突问题

--- transcode.py
+++ transcode.py
@@ -9,12 +9,12 @@ while True:
     config_files = os.listdir(config.CONFIG_PATH)
     if not encode_files:
         print("NOT FOUND file to transcode")
-    for file in encode_files:
-        if config.RECORD_FORMAT in file:
+    for encode_file in encode_files:
+        if config.RECORD_FORMAT in encode_file:
             print("Check and transcode the record file: {file}")
             # transcode
-            new_file = file[:-len(config.RECORD_FORMAT)] + config.TRANSCODE_FORMAT
-            cmd = f"ffmpeg -i {config.ENCODE_PATH}/{file} -y -c:v copy -c:a copy {config.ENCODE_PATH}/{new_file}"
+            new_file = encode_file[:-len(config.RECORD_FORMAT)] + config.TRANSCODE_FORMAT
+            cmd = f"ffmpeg -i {config.ENCODE_PATH}/{encode_file} -y -c:v copy -c:a copy {config.ENCODE_PATH}/{new_file}"
             os.system(cmd)
             # move files
             for config_file in config_files:
